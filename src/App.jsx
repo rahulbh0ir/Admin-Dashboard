12ebfb1c626ba55191e5bc5c0ca93109970ddc7c
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-03-21 10:38:56 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import Home from "./pages/home/Home"
+import Products from "./pages/products/Products"
+import Users from "./pages/users/Users"
+import { createBrowserRouter, RouterProvider, Route, Link} from "react-router-dom"
+
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Home />  
+    },
+    {
+      path:"/users",
+      element: <Users />  
+    },
+    {
+      path:"/products",
+      element: <Products />  
+    }
+  ])
+
   return (
-    <div>Hello World</div>
+   <RouterProvider router={router} />
   )
 }
 
