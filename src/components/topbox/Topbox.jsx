6eec86ecbf2344c8a462e8a0068498162d9e0b98
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-04-09 10:35:40 +0530

    up

---
+++ src/components/topbox/Topbox.jsx
@@ -0,0 +1,11 @@
+import "./topbox.scss";
+
+const Topbox = () => {
+  return (
+    <div className="topbox">
+      Topbox
+    </div>
+  )
+}
+
+export default Topbox
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Topbox from "../../components/topbox/Topbox"
 import "./home.scss"
 
 const Home = () => {
   return (
     <>
       <div className="home">
-        <div className="box box1">Box 1</div>
+        <div className="box box1">
+          <Topbox />
+        </div>
         <div className="box box2">Box 2</div>
         <div className="box box3">Box 3</div>
         <div className="box box4">Box 4</div>
