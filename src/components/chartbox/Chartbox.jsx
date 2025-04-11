a4e7bc647b6c73fc6dc2e86dc40b6c9cf4125186
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-04-11 17:04:43 +0530

    up

--- src/components/chartbox/Chartbox.jsx
+++ src/components/chartbox/Chartbox.jsx
@@ -2,73 +2,30 @@ import { Link } from "react-router-dom"
 import "./chartbox.scss"
 import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts"
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
-const Chartbox = () => {
+const Chartbox = (props) => {
   return (
     <div className="chartbox">
       <div className="boxinfo">
         <div className="title">
-          <img src="./user.svg" />
-          <span>Total Users</span>
+          <img src={props.icon} />
+          <span>{props.title}</span>
         </div>
-        <h1>11.38</h1>
+        <h1>{props.numbers}</h1>
         <Link to="/">View All</Link>
       </div>
       <div className="chartinfo">
         <div className="chart">
           <ResponsiveContainer width="100%" height="100%">
-            <LineChart width={300} height={100} data={data}>
+            <LineChart width={300} height={100} data={props.chartdata}>
               <Tooltip 
                 contentStyle={{background: "transparent", border: "none"}}
                 labelStyle={{display : "none"}}
+                position={{x : 10 , y : 70 }}
               />
               <Line type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
+                dataKey={props.dataKey}
+                stroke={props.color}
                 strokeWidth={2}
                 dot={false}
               />
@@ -76,7 +33,7 @@ const Chartbox = () => {
           </ResponsiveContainer>
         </div>
         <div className="texts">
-          <div className="percentage">45%</div>
+          <div className="percentage">{props.percentage}%</div>
           <div className="duration">this month</div>
         </div>
       </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
-import Chartbox from "../../components/chartbox/Chartbox"
-import Topbox from "../../components/topbox/Topbox"
-import "./home.scss"
+import Chartbox from "../../components/chartbox/Chartbox";
+import Topbox from "../../components/topbox/Topbox";
+import "./home.scss";
+import { chartBoxUser, chartBoxProduct, chartBoxRevenue, chartBoxConversion } from "../../data"
 
 const Home = () => {
   return (
@@ -9,11 +10,11 @@ const Home = () => {
         <div className="box box1">
           <Topbox />
         </div>
-        <div className="box box2"><Chartbox /></div>
-        <div className="box box3"><Chartbox /></div>
+        <div className="box box2"><Chartbox {...chartBoxUser}  /></div>
+        <div className="box box3"><Chartbox {...chartBoxProduct} /></div>
         <div className="box box4">Box 4</div>
-        <div className="box box5"><Chartbox /></div>
-        <div className="box box6"><Chartbox /></div>
+        <div className="box box5"><Chartbox {...chartBoxConversion}/></div>
+        <div className="box box6"><Chartbox {...chartBoxRevenue} /></div>
         <div className="box box7">Box 7</div>
         <div className="box box8">Box 8</div>
         <div className="box box9">Box 9</div>
