6577e2d53424ae4f44f48fcbfbfc12f27ccfe1df
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-04-18 15:00:06 +0530

    up

--- src/components/tabledata/Tabledata.jsx
+++ src/components/tabledata/Tabledata.jsx
@@ -1,103 +1,57 @@
 import "./tabledata.scss";
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
-
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: "avatar" , headerName: "Avatar", width: 100,
-    renderCell: (params) => {
-      return (
-        <img src={params.row.img || "./noavatar.png"} alt="" />
-      )
-    }
-  },
-  {
-    field: ""
-  },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'email',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'phone',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'created',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'verified',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'action',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  
-];
+import { Link } from "react-router-dom";
 
 // valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
+const Tabledata = (props) => {
 
 
+  const handleDelete = (a) => {
+    console.log(a + " Deleted")
+  }
 
+  const actionColumn = {
+    field: 'action',
+    headerName: 'Action',
+    width: 120,
+    renderCell: (x) => {
+      return (
+        <div className="actions">
+          <Link>
+          <img src="./view.svg" alt="" />
+          </Link>
+          <img src="./delete.svg" alt="" onClick={() => handleDelete(x.row.id)}/>
+        </div>
+      )
+    }
+  }
 
-
-
-const Tabledata = () => {
   return (
     <div className="tabledata">
       <Paper sx={{ height: "100%", width: '100%' }}>
         <DataGrid
           className="grid"
-          rows={rows}
-          columns={columns}
-          // initialState={{ pagination: { paginationModel } }}
+          rows={props.rows}
+          columns={[...props.columns, actionColumn]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            }
+          }}
           pageSizeOptions={[5, 10]}
           checkboxSelection
           sx={{ border: 0 }}
-          slots={{toolbar: GridToolbar}}
+          slots={{ toolbar: GridToolbar }}
           slotProps={{
             toolbar: {
               showQuickFilter: true,
-              quickFilterProps: {debounceMs: 500},
-            }  
+              quickFilterProps: { debounceMs: 500 },
+            }
           }}
           disableColumnFilter
           disableDensitySelector
--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,6 +1,63 @@
 import Tabledata from "../../components/tabledata/Tabledata";
 import "./users.scss"
-import * as React from 'react';
+import { userRows } from "./../../data"
+
+const columns = [
+  {
+    field: 'id',
+    headerName: 'ID',
+    width: 90
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return (
+        <img src={params.row.img || "./noavatar.png"} alt="" />
+      )
+    }
+  },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    width: 250,
+    editable: true,
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone Number',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Created At',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'verified',
+    headerName: 'Verified',
+    type: "boolean",
+    width: 150,
+    editable: true,
+  },
+
+];
+
 
 
 const Users = () => {
@@ -12,7 +69,7 @@ const Users = () => {
           <button>Add</button>
         </div>
         <div className="table">
-         <Tabledata />
+          <Tabledata columns={columns} rows={userRows} />
         </div>
       </div>
 
