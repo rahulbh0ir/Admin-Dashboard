db3fc8610607e45147d44e83a4f013d2e7c66df1
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-04-18 11:46:30 +0530

    up

--- src/components/tabledata/Tabledata.jsx
+++ src/components/tabledata/Tabledata.jsx
@@ -1,17 +1,39 @@
 import "./tabledata.scss";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: "avatar" , headerName: "Avatar", width: 100,
+    renderCell: (params) => {
+      return (
+        <img src={params.row.img || "./noavatar.png"} alt="" />
+      )
+    }
+  },
+  {
+    field: ""
+  },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
   {
     field: 'age',
     headerName: 'Age',
     type: 'number',
-    width: 90,
+    width: 110,
+    editable: true,
   },
   {
     field: 'fullName',
@@ -24,10 +46,10 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
@@ -35,7 +57,7 @@ const rows = [
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-const paginationModel = { page: 0, pageSize: 10 };
+
 
 
 
@@ -48,10 +70,20 @@ const Tabledata = () => {
           className="grid"
           rows={rows}
           columns={columns}
-          initialState={{ pagination: { paginationModel } }}
+          // initialState={{ pagination: { paginationModel } }}
           pageSizeOptions={[5, 10]}
           checkboxSelection
           sx={{ border: 0 }}
+          slots={{toolbar: GridToolbar}}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: {debounceMs: 500},
+            }  
+          }}
+          disableColumnFilter
+          disableDensitySelector
+          disableColumnSelector
         />
       </Paper>
     </div>
