723711dc848ba83f3d1cd6d9998e632049d89e4a
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-04-21 10:36:57 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
       element: <Layout />,
       children: [
         {
-          path:"/",
+          path:"/Admin-Dashboard",
           element: <Home />
         },
         {
